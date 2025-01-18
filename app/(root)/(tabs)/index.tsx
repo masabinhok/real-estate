c2d1bb0ad0dab90f6f3bbdacd2690ef10eabd742
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-18 18:04:59 +0545

    feat: update user greeting in Index layout with dynamic avatar and name from global context

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,10 +3,12 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const { user } = useGlobalContext();
   return (
     <SafeAreaView className="h-full bg-white">
       <FlatList
@@ -20,10 +22,10 @@ export default function Index() {
         ListHeaderComponent={<View className="px-5">
           <View className="flex flex-row items-center justify-between mt-5">
             <View className="flex flex-row">
-              <Image source={images.avatar} className="size-12 rounded-full" />
+              <Image source={{ uri: user?.avatar }} className="size-12 rounded-full" />
               <View className="flex flex-col items-start ml-2 justify-center">
                 <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-                <Text className="text-base font-rubik-medium text-black-300">Sabin</Text>
+                <Text className="text-base font-rubik-medium text-black-300">{user?.name}</Text>
               </View>
             </View>
             <Image className="size-6" source={icons.bell} />
