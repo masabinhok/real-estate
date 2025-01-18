0e084111b4b45a5663991707df7ad7c70a198344
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-18 16:40:51 +0545

    feat: add Search component with debounced input and update Index layout with user greeting

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,16 +1,27 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-rubik text-3xl mb-5">Welcome to Real-Estate</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <View className="px-5">
+
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+              <Text className="text-base font-rubik-medium text-black-300">Sabin</Text>
+            </View>
+          </View>
+          <Image className="size-6" source={icons.bell} />
+        </View>
+      </View>
+      <Search />
+
+    </SafeAreaView>
   );
 }
