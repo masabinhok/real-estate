95000f43d248900ddcbb20be1a7e577c98244390
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2025-01-18 18:03:38 +0545

    feat: refactor Index layout to use FlatList for featured items and recommendations; update header and integrate Filters component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,55 +3,61 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView className="h-full bg-white">
-      <View className="px-5">
-
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row">
-            <Image source={images.avatar} className="size-12 rounded-full" />
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
-              <Text className="text-base font-rubik-medium text-black-300">Sabin</Text>
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={() => <Card />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={<View className="px-5">
+          <View className="flex flex-row items-center justify-between mt-5">
+            <View className="flex flex-row">
+              <Image source={images.avatar} className="size-12 rounded-full" />
+              <View className="flex flex-col items-start ml-2 justify-center">
+                <Text className="text-xs font-rubik text-black-100">Good Morning</Text>
+                <Text className="text-base font-rubik-medium text-black-300">Sabin</Text>
+              </View>
             </View>
+            <Image className="size-6" source={icons.bell} />
           </View>
-          <Image className="size-6" source={icons.bell} />
-        </View>
-        <Search />
-        <View className="my-5 ">
+          <Search />
+          <View className="my-5 ">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Featured
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+              </TouchableOpacity>
+            </View>
+            <FlatList
+              horizontal
+              data={[1, 2, 3]}
+              renderItem={() => <FeaturedCard />}
+              keyExtractor={(item) => item.toString()}
+              showsHorizontalScrollIndicator={false}
+              bounces={false}
+              contentContainerClassName="flex gap-5 mt-5"
+            />
+          </View>
           <View className="flex flex-row items-center justify-between">
             <Text className="text-xl font-rubik-bold text-black-300">
-              Featured
+              Our Recommendation
             </Text>
             <TouchableOpacity>
               <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
             </TouchableOpacity>
           </View>
-          <View className="flex flex-row gap-5 mt-5">
-            <FeaturedCard />
-            <FeaturedCard />
-            <FeaturedCard />
-          </View>
-        </View>
-        <View className="flex flex-row items-center justify-between">
-          <Text className="text-xl font-rubik-bold text-black-300">
-            Our Recommendation
-          </Text>
-          <TouchableOpacity>
-            <Text className="text-base font-rubik-bold text-primary-300">See All</Text>
-          </TouchableOpacity>
-        </View>
-        <Filters />
-        <View className="flex flex-row gap-5 mt-5">
-          <Card />
-          <Card />
-        </View>
-      </View>
-
+          <Filters />
+        </View>} />
     </SafeAreaView>
   );
 }
